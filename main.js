8d06bc90819e0253b1a173ca88581f4e9cef7660
paulo007.sn <paulo007.sn@gmail.com>
2025-03-25 21:37:15 +0000

    função para receber valor da conta (bill) e função para receber o número de pessoas

---
+++ main.js
@@ -0,0 +1,12 @@
+let bill = 0
+let tipPercentage = 0
+let numberOfPeople = 0
+
+function receiveBillValue(){
+    bill = document.querySelector("#bill").valueAsNumber
+
+}
+
+function reciveNumberOfPeopleValue(){
+    numberOfPeople = document.querySelector("#people").valueAsNumber
+}
