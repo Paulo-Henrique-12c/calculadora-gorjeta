77722c42189826aa0d08e9633d992e169223f48f
paulo007.sn <paulo007.sn@gmail.com>
2025-03-30 23:56:34 +0000

    função para receber o valor customizado da porcentagem da gorjeta

--- main.js
+++ main.js
@@ -5,11 +5,13 @@ let buttonSelected = null
 
 function receiveBillValue(){
     bill = document.querySelector("#bill").valueAsNumber
+    calculate()
 
 }
 
 function reciveNumberOfPeopleValue(){
     numberOfPeople = document.querySelector("#people").valueAsNumber
+    calculate()
 }
 
 function reciveTipPercentageValue(value){
@@ -28,15 +30,28 @@ function reciveTipPercentageValue(value){
 
   buttonSelected = document.querySelector(`#button-${value}`)
   buttonSelected.classList.add("button-selected")
+  calculate()
 }
 
-function reciveNumberOfPeopleValue(){
+function recriveCustomTipPercentageValue(){
   if(buttonSelected !== null){
     buttonSelected.classList.remove("button-selected")
     buttonSelected = null
   }
    
   tipPercentage = document.querySelector("#custom-tip").valueAsNumber / 100
+  calculate()
+}
 
+function calculate(){
+if(bill !== 0 && tipPercentage !== 0 && numberOfPeople !== 0){
+  let amountStrong = document.querySelector(".amount strong")
+  let tipAmountPerson = (bill * tipPercentage) / numberOfPeople
+  amountStrong.innerText = `$${tipAmountPerson.toFixed(2)}`
 
+  let totalStrong = 
+}
+  else{
+    console.log("preencha tudo")
+  }
 }
